83ca599e19a52860c554e8ca784cf5d969d44910
AbsraGetahun <absraabby@gmail.com>
2026-08-18 10:08:16 +0300

    Fixed Dockerfile with all directories and simplified routes

--- jobportal-backend/routes/web.php
+++ jobportal-backend/routes/web.php
@@ -1,11 +1,11 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return 'API is running!';
-});
-
-Route::get('/ping', function () {
-    return 'pong';
-});
+<?php  
+  
+use Illuminate\Support\Facades\Route;  
+  
+Route::get("/", function () {  
+    return "Job Portal API is running!";  
+});  
+  
+Route::get("/ping", function () {  
+    return "pong";  
+}); 
