95dc89bfe3eca78b4139721b5cd24ec366b9445c
AbsraGetahun <absraabby@gmail.com>
2026-08-18 20:29:53 +0300

    Add Netlify URL to CORS allowed origins

--- jobportal-backend/config/cors.php
+++ jobportal-backend/config/cors.php
@@ -15,11 +15,17 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '/register', 'admin/*', '/notifications', '/profile'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:8000', 'http://127.0.0.1:8000'],
+    'allowed_origins' => [
+        'https://sprightly-selkie-98917e.netlify.app',
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'http://localhost:8000',
+        'http://127.0.0.1:8000',
+    ],
 
     'allowed_origins_patterns' => [],
 
